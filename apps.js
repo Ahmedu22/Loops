3a860e5a6060a13611ce7ba8f0378f81869bcf72
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-19 12:05:20 +0300

    loops

---
+++ apps.js
@@ -0,0 +1,32 @@
+// Loops
+// types of loops 
+// for
+// while 
+// do while 
+
+// 1. for loop
+
+// for(let i = 0; i <= 200; i++){
+//     console.log("Ahmed", i);
+// }
+
+for(let i = 1; i <= 10; i++){
+    console.log("Ahmed ", i);
+}
+
+// 2. while loop
+
+var a = 5;
+while(a <= 10){
+    console.log("yussuf", a);
+    a++;
+}
+
+
+// 3. do while
+
+let y = 10;
+ do{
+    console.log("ibrahim", y);
+    y++;
+ }while(y <= 20);
